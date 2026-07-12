3c43140ad4ad02c61e02d613362d907a35788956
johncarlangelo <johncarlbulaon@gmail.com>
2026-07-13 03:41:39 +0800

    fix: change vite base to relative path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Hermanos-Arsenal/',
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
